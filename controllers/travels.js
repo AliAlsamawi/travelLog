4c14ae3cfe032430908b230fa8211093c01ce611
Ali A <alsamawiali4@gmail.com>
2021-11-07 16:28:57 -0500

    travel funtion

--- controllers/travels.js
+++ controllers/travels.js
@@ -1,6 +1,7 @@
 import { Travel } from "../models/travel.js"
 
 
+
 //Get all travelogs
 function index(req, res) {
   console.log(req.user, "user")
@@ -18,6 +19,50 @@ function index(req, res) {
     })
 }
 
-export {
-index,
+function createTravelLog(req, res) {
+  res.render("travels/new", { title: "Create New Log" })
+}
+
+//create a travel log
+function addTravelLog(req, res) {
+  console.log(req.body)
+  const body = {
+    title: req.body.title,
+    rating: req.body.rating,
+    cost: req.body.cost,
+    test: req.body.test,
+    worththetrip: req.body.worththetrip,
+    location: req.body.location,
+    author: {
+      id: req.user.profile._id,
+      name: req.user.profile.name,
+    },
+  }
+  Travel.create(body)
+    .then((travel) => {
+      res.redirect("/travels")
+    })
+    .catch((err) => {
+      res.redirect("/")
+    })
+}
+
+function getTravelLog(req, res) {
+  Travel.findById(req.params.id)
+    .populate("reviews")
+    .then((travel) => {
+      res.render("travels/show", {
+        title: "Show Page",
+        travel,
+      })
+    })
+    .catch((err) => {
+      res.json(err)
+    })
+}
+
+export { index, 
+  getTravelLog, 
+  createTravelLog, 
+  addTravelLog, 
 }
--- routes/travels.js
+++ routes/travels.js
@@ -6,6 +6,10 @@ import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 router.get('/', travelsCtrl.index)
+router.get("/new", travelsCtrl.createTravelLog)
+router.get("/:id", travelsCtrl.getTravelLog)
+
+router.post("/", isLoggedIn, travelsCtrl.addTravelLog) //create new travel log
 
 export {
   router
