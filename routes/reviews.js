c40e749ecdc45292bf62d6c74fb6f099a98222dd
Ali A <alsamawiali4@gmail.com>
2021-11-08 11:16:55 -0500

    fix form edit button

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,7 +7,7 @@ import { isLoggedIn } from '../middleware/middleware.js'
 const router = Router()
 router.post("/:id/reviews", isLoggedIn, reviewsCtrl.createReview)
 
-// router.get("/:id/reviews/:review_id", isLoggedIn,reviewsCtrl)
+router.get("/:id/reviews/:review_id", isLoggedIn,reviewsCtrl)
 
 router.delete("/:id/reviews/:review_id", reviewsCtrl.delete)
 
